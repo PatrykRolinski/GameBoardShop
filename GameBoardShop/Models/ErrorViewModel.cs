24ba862911b72dccba110b4c57bbb87ec05f7a54
Patryk <rolinski.patryk@gmail.com>
2022-11-19 13:37:42 +0100

    Add project files.

---
+++ GameBoardShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GameBoardShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
