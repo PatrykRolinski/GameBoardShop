7ea5b0c87186b328c78eeb02c2c2694113145b20
Patryk <rolinski.patryk@gmail.com>
2022-12-13 20:37:21 +0100

    Add IntegrationTests.csproj, Add CustomWebApplicationFactory

---
+++ IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+
+namespace IntegrationTests;
+
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+
+            // Remove GameBoardShopContext
+            services.RemoveDbContext<GameBoardShopContext>();
+
+            //Add DbContext
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            services.AddDbContext<GameBoardShopContext>(options => options.UseSqlite(connection));
+
+            //Ensure schema get created
+            services.EnsureDbCreated<GameBoardShopContext>();
+
+        });
+    }
+}
---
+++ IntegrationTests/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace IntegrationTests;
+
+public static class ServiceCollectionExtensions
+{
+    public static void RemoveDbContext<T> (this IServiceCollection services) where T : DbContext
+    {
+        var desciptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<T>));
+        if(desciptor != null) services.Remove(desciptor);
+    }
+
+    public static void EnsureDbCreated<T>(this IServiceCollection services) where T: DbContext
+    {
+        var serviceProvider = services.BuildServiceProvider();
+
+        using var scope = serviceProvider.CreateScope();
+        var scopedServices = scope.ServiceProvider;
+        var context = scopedServices.GetRequiredService<T>();
+        context.Database.EnsureCreated();
+    }
+}
